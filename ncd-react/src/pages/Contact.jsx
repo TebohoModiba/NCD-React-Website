5165d5e50de23076560165fdc3f4d88228604136
Teboho Modiba <tebohomodiba658@gmail.com>
2026-09-03 11:03:04 +0200

    Remove contact numbers from Contact page
    Removed contact numbers for Inako Bukani and Teboho M. Modiba.

--- ncd-react/src/pages/Contact.jsx
+++ ncd-react/src/pages/Contact.jsx
@@ -17,7 +17,6 @@ export default function Contact() {
         <p><strong>Email:</strong> inakoelubabalo@gmail.com</p>
         <p><strong>TikTok:</strong> <a href="https://www.tiktok.com/@inako_b1?_t=ZS-90q5tAcnXBF&_r=1" target="_blank" rel="noopener noreferrer">@Inako_b1</a></p>
         <p><strong>Facebook:</strong> <a href="https://www.facebook.com/share/1FfeHCYde5/" target="_blank" rel="noopener noreferrer">Inako Bukani</a></p>
-        <p><strong>Contact Number</strong> +27 63 958 5518</p>
       </div>
 
       <div className="Teboho-contact-info">
@@ -25,7 +24,6 @@ export default function Contact() {
         <p><strong>Email:</strong> tebohomodiba658@gmail.com</p>
         <p><strong>TikTok:</strong> <a href="https://www.tiktok.com/@tebza5533" target="_blank" rel="noopener noreferrer">@tebza5533</a></p>
         <p><strong>Facebook:</strong> <a href="https://www.facebook.com/teboho.mahlatse.modiba" target="_blank" rel="noopener noreferrer">Teboho M. Modiba</a></p>
-        <p><strong>Contact Number</strong> +27 66 096 3925</p>
       </div>
     </>
   );
