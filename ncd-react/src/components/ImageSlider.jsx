10261606afa3abbf9bdf6a280c504e1251002cd7
Teboho Mahlatse Modiba <st10444598@myemeris.edu.za>
2026-09-06 01:38:25 +0200

    Updated image name in the ImageSlider.jsx file

--- ncd-react/src/components/ImageSlider.jsx
+++ ncd-react/src/components/ImageSlider.jsx
@@ -4,7 +4,7 @@ const slides = [
   '/assets/Miss Trans Diva.jpg',
   '/assets/Qhama Birthday Poster.jpg',
   '/assets/Miss Teenable.jpg',
-  '/assets/Mdantsane LGBTQIA+ Safe Space And Braai.jpg'
+  '/assets/Mdantsane-LGBTQIA-Safe-Space-and-Braai'
 ];
 
 export default function ImageSlider() {
